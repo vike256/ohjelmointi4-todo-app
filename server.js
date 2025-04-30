c7205461c7af80d9924fa88ecc5f572277ce2b88
vike256 <69812383+vike256@users.noreply.github.com>
2025-04-30 21:54:50 +0300

    Better wording

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
-const crypto = require('crypto')  // Used for creating UUIDs for tasks
+const crypto = require('crypto')  // Used to create UUIDs for tasks
 
 const app = express()
 const PORT = 3000
