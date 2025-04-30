9d1e733f19c37f6a3389229044fbe24ec6d1fc07
vike256 <69812383+vike256@users.noreply.github.com>
2025-04-30 21:04:17 +0300

    Add get and post functionality

--- server.js
+++ server.js
@@ -1,18 +1,63 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const crypto = require('crypto')
+
 const app = express()
 const PORT = 3000
-const tasksFilePath = path.join(__dirname, 'tasks.json')
+const tasks = require('./tasks.json')
 
+
+function updateTasksFile() {
+    fs.writeFile(
+        'tasks.json',
+        JSON.stringify(tasks),
+        err => {
+            if (err) throw err
+            
+            console.log('tasks.json file updated')
+        }
+    )
+}
+
+app.use(express.json())
 
 // Get tasks
 app.get('/api/tasks', (req, res) => {
-    res.sendFile(tasksFilePath)
+    res.send(tasks)
 })
 
 // Add task
 app.post('/api/tasks', (req, res) => {
+    if (!req.is('application/json')) {
+        res.status(415).json({ error: "Content-Type must be application/json" })
+        return
+    }
 
+    const { name, category } = res.body
+
+    if (!name) {
+        res.status(400).json({ error: "Task has no name" })
+        return
+    }
+
+    if (category !== 0 || category !== 1) {
+        res.status(400).json({ error: "Category must be 0 or 1" })
+        return
+    }
+
+    let newTask = {
+        id: crypto.randomUUID(),
+        name: name,
+        category: category,
+        priority: res.body.priority || 1,
+        date: res.body.date || ""
+    }
+
+    tasks.push(newTask)
+    updateTasksFile()
+
+    res.sendStatus(200).json({ message: "Task added" })
 })
 
 // Update task
@@ -26,5 +71,5 @@ app.delete('/api/tasks', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+    console.log(`http://localhost:${PORT} \nhttp://localhost:${PORT}/api/tasks`)
 })
